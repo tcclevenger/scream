ebec516133cb786aff62863048fb76a83c85c597
Benjamin Hillman <bhillma@sandia.gov>
2020-07-30 17:45:22 -0600

    Build cloud optics and test fluxes
    Build the RRTMGP cloud optics interface so we can replicate the
    example all-sky problem, and add a test to compare fluxes against
    reference calculation. This fails for now, because we have not
    implemented the full all-sky problem, but will be a good benchmark
    as we add functionality.

--- components/scream/src/physics/rrtmgp/scream_rrtmgp_interface.hpp
+++ components/scream/src/physics/rrtmgp/scream_rrtmgp_interface.hpp
@@ -4,6 +4,7 @@
 #include "ekat/scream_assert.hpp"
 #include "ekat/util/scream_utils.hpp"
 #include "mo_gas_optics_rrtmgp.h"
+#include "mo_fluxes.h"
 
 namespace scream {
     namespace rrtmgp {
@@ -18,13 +19,15 @@ namespace scream {
         extern void rrtmgp_main(
                 real2d &p_lay, real2d &t_lay, real2d &p_lev, real2d &t_lev, 
                 GasConcs &gas_concs, real2d &col_dry,
-                real2d &sfc_alb_dir, real2d &sfc_alb_dif, real1d &mu0);
+                real2d &sfc_alb_dir, real2d &sfc_alb_dif, real1d &mu0,
+                FluxesBroadband &fluxes_sw, FluxesBroadband &fluxes_lw);
         extern void rrtmgp_finalize();
         extern void rrtmgp_sw(
                 GasOpticsRRTMGP &k_dist, 
                 real2d &p_lay, real2d &t_lay, real2d &p_lev, real2d &t_lev, 
                 GasConcs &gas_concs, real2d &col_dry, 
-                real2d &sfc_alb_dir, real2d &sfc_alb_dif, real1d &mu0);
+                real2d &sfc_alb_dir, real2d &sfc_alb_dif, real1d &mu0,
+                FluxesBroadband &fluxes);
     } // namespace rrtmgp
 }  // namespace scream
 
